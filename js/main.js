f2ca652132d5cd4cb6869fce5bdd5b4b76efa08a
robs-am <roberta.amaro89@gmail.com>
2022-05-23 18:17:09 -0300

    add getElementby js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ const characterAmountRange = document.getElementById
 const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
 
+const uppercaseElement = document.getElementById
+('uppercase')
+
+const numbersElement = document.getElementById
+('numbers')
+
+const symbolsElement = document.getElementById
+('symbols')
+
 const form = document.getElementById('passwordGeneratorForm')
 
 
@@ -12,10 +21,17 @@ characterAmountRange.addEventListener('input', syncCharacterAmount)
 
 form.addEventListener('submit',e=>{
     e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const uppercase = uppercaseElement.checked
+    const numbers = numbersElement.checked
+    const symbols = symbolsElement.checked
+    
+    
+    const password = generatePassword(characterAmount, uppercase, numbers, symbols)
   })
 
 function syncCharacterAmount(e){
-    const value=e.target.value
+    const value = e.target.value
     characterAmountNumber.value = value
     characterAmountRange.value = value
    }
