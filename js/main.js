60be417fe97d6bd9f14b2ea2d4973f3e9769c743
robs-am <roberta.amaro89@gmail.com>
2022-05-23 19:53:03 -0300

    add js final

--- js/main.js
+++ js/main.js
@@ -8,40 +8,43 @@ const uppercaseElement = document.getElementById
 ('uppercase')
 
 const numbersElement = document.getElementById
-('numbers')
+('number')
 
 const symbolsElement = document.getElementById
-('symbols')
+('symbol')
 
 const form = document.getElementById('passwordGeneratorForm')
 
 /* FUNÇÃO */
 
-function generatePassword(characterAmount, uppercase, numbers, symbols) {
-
+function generatePassword(characterAmount, uppercase, number, symbol) {
+    console.log(LOWERCASE_CHAR_CODES)
 }
 
-function syncCharacterAmount(e) {
-    const value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
-}
+
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES= arrayFromLowToHigh(48,57)
+const SYMBOL_CHAR_CODES= arrayFromLowToHigh(33,47).concat(
+    arrayFromLowToHigh(58,64)
+  ).concat(
+    arrayFromLowToHigh(91,96)
+  ).concat(
+    arrayFromLowToHigh(123,126)
+  )
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-form.addEventListener('submit',e=>{
+form.addEventListener('submit', e => {
     e.preventDefault()
     const characterAmount = characterAmountNumber.value
     const uppercase = uppercaseElement.checked
     const numbers = numbersElement.checked
     const symbols = symbolsElement.checked
-    
-    
     const password = generatePassword(characterAmount, uppercase, numbers, symbols)
+    passwordDisplay.innerText = password
   })
 
   
